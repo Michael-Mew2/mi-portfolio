9e781f3e7dc43b354cd933522d7d40f1aaebae34
dci-student <mmn.umfragen@gmail.com>
2025-03-12 18:12:22 +0100

    [🐛 bugfix]: repair contact-form
    improved email-text

--- api/send-email.js
+++ api/send-email.js
@@ -17,12 +17,26 @@ export default async function (req, res) {
 
   try {
     const info = await transporter.sendMail({
-      from: `"mi - automatic response 🤖" <${process.env.EMAIL}>`,
+      from: `"mi (michael's internet-Portfolio) - automatic response 🤖" <${process.env.EMAIL}>`,
       to: email,
       bcc: [process.env.COPY, process.env.EMAIL],
       subject: `Hi ${fname} ${lname} - Thank you for reaching out to me 😃`,
-      text: `Hello ${fname} ${lname}, ...`, // Verkürzte E-Mail
-      html: `<h2>Hello ${fname} ${lname},</h2><p>Thank you for reaching out...</p>`,
+            text: `Moin (northern-german for "hi) ${fname} ${lname}, Thank you so much for getting in touch! I’ve received your message and will get back to you shortly. Here’s a quick look at the details you shared, just to keep everything transparent; Name: ${fname} ${lname}, company: ${company}, Phone Number: ${tel}, Email: ${email}, (Method: ${method}), Message: ${reason}. II’ll get back to you within a day or two, but if anything above needs correcting, please feel free to reach me directly at contact.michaels.website@gmail.com. Thanks again, and I look forward to connecting with you soon! Best regards Michael`,
+            html: `
+            <h2>Moin (= northern-german for "hi") ${fname} ${lname},</h2>
+            <p>Thank you so much for getting in touch! I’ve received your message and will get back to you shortly. Here’s a quick look at the details you shared, just to keep everything transparent:</p>
+            <ul>
+            <li><strong>Name:</strong> ${fname} ${lname},</li>
+            <li><strong>Company:</strong> ${company},</li>
+            <li><strong>Phone Number:</strong> ${tel},</li>
+            <li><strong>Email:</strong> ${email},</li>
+            <li>(<strong>Prefered Contact Method:</strong> ${method}),</li>
+            <li><strong>Message:</strong> ${reason}.</li>
+            </ul>
+            <p>I’ll get back to you within a day or two, but if anything above needs correcting, please feel free to reach me directly at <a href="mailto:contact.michaels.website@gmail.com">contact.michaels.website@gmail.com</a>.</p>
+            <p>Thanks again, and I look forward to connecting with you soon!</p>
+            <p>Best regards</p>
+            <b>Michael 🤓</b>`,
     });
 
     res.status(200).json({ success: true, messageId: info.messageId });
